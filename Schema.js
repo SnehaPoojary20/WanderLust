59bbca54bbf9f8184a6e288d6fd371de874de6f2
Sneha Poojary <snehapoojary2004@gmail.com>
2024-12-10 10:36:41 +0530

    Add existing file

---
+++ Schema.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+ module.exports.listingSchema=Joi.object({
+    listing:Joi.object({
+        title:Joi.string().required,
+        description:Joi.string().required,
+        Image:Joi.string().allow("",null),
+        Price:Joi.number().min(0),
+        Location:Joi.string().required,
+        Country:Joi.string().required
+    }).required(),
+
+});
+
+module.exports.reviewSchema=Joi.object({
+    listing:Joi.object({
+       rating:Joi.number().required().min(1).max(5),
+       Comment:Joi.string().required,
+
+    }).required(),
+});
